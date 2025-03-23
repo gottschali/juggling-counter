2715a004e050a2f67fbc3254554ef75d7b87d82e
Gottschali <aligottschall@protonmail.com>
2025-03-23 20:09:47 +0100

    add more icons from svgrepo
    https://www.svgrepo.com/collection/interface-icons/

---
+++ src/icons/InfoIcon.svelte
@@ -0,0 +1,16 @@
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="#000000"
+    width="256px"
+    height="256px"
+    viewBox="0 0 24 24"
+    ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    ></g><g id="SVGRepo_iconCarrier"
+        ><path
+            d="M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z"
+        ></path></g
+    ></svg
+>

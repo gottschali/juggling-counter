fa5d63e540340eb613aa8708c2099b6f6f826970
Gottschali <aligottschall@protonmail.com>
2025-03-23 21:16:27 +0100

    draft ball list

--- src/App.svelte
+++ src/App.svelte
@@ -11,18 +11,16 @@
 
     let count: number = $state(0);
 
-
     const setCount = (c: number) => {
         count = c;
     };
-    const addSequence = (s: Sequence) => sequences = new Array(s, ...sequences);
+    const addSequence = (s: Sequence) =>
+        (sequences = new Array(s, ...sequences));
 
     // could display live statistics for the current sequence
     // let sequence = $state(new Sequence());
     let sequences: Sequence[] = $state([]);
     // let debugSeq = $derived(JSON.stringify(sequence));
-
-
 </script>
 
 <main>
@@ -32,19 +30,30 @@
         <IconButton icon={InfoIcon} />
         <IconButton icon={SettingsIcon} />
     </div> -->
+    <ul>
+        <li>Count your juggling catches automatically based on audio only.</li>
+        <li>
+            Permission to access the microphone must be given. All processing
+            happens on your device.
+        </li>
+        <li>
+            Works only in quiet environment and if you juggle <i>loud</i> and
+            <i>regular</i> enough.
+        </li>
+    </ul>
     <div>
-        <Recorder setCount={setCount} addSequence={addSequence} />
+        <Recorder {setCount} {addSequence} />
     </div>
     <div>
-        <Upload setCount={setCount} addSequence={addSequence} />
+        <Upload {setCount} {addSequence} />
     </div>
     <div
-            style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
-        >
-            {#if count % 10 == 0}
-                <Confetti amount={count} rounded={true} />
-            {/if}
-        </div>
+        style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
+    >
+        {#if count % 10 == 0}
+            <Confetti amount={count} rounded={true} />
+        {/if}
+    </div>
     <h2 title="current number of catches">{count}</h2>
     <LogTable {sequences} />
 </main>
@@ -53,5 +62,12 @@
     main {
         text-align: center;
         padding: 20px;
+    }
+    li {
+        /* TODO proper ball and size */
+        list-style-image: url(/ball.svg);
+        border-radius: 1ch;
+        padding-inline: 1ch;
+        margin-inline-end: 1ch;
     }
 </style>
